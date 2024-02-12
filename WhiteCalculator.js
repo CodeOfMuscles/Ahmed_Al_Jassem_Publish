6cf230b41d58ce2afb853752a497943f7cf749aa
CodeOfMuscles <146719124+CodeOfMuscles@users.noreply.github.com>
2024-02-12 22:55:09 +0300

    Update WhiteCalculator.js

--- WhiteCalculator.js
+++ WhiteCalculator.js
@@ -75,7 +75,7 @@ buttons.forEach(function(ele){
                 -5px -5px 12px rgb(255,255,255,0.06)
                 `
         });
-        moImg.children[0].src = "WhiteCalmoon1.png"
+        moImg.children[0].src = "moon1.png"
         booleanVOfMoon = false
         localStorage.setItem("stuts","dark");
         
@@ -109,7 +109,7 @@ buttons.forEach(function(ele){
           background: ${mainCol};
           `
         })
-        moImg.children[0].src = "WhiteCalmoon.png"
+        moImg.children[0].src = "moon.png"
 
         booleanVOfMoon = true;
         localStorage.setItem("stuts","light")
